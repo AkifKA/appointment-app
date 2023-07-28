fe3315cf620a4ac4641cf004b66235e66d7cdef5
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-07-28 12:22:38 +0300

    3. Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Doctors from "../components/Doctors";
+import AppointmentList from "../components/AppointmentList";
+import { doctorData } from "../helper/data";
 
 const Home = () => {
-  return <main>HOME</main>;
+  const [doctors, SetDoctors] = useState(doctorData);
+
+  return (
+    <main className="text-center mt-2 vh-100">
+      <h1 className="display-5 text-success">MAK HOSPITAL</h1>
+      <Doctors doctors={doctors} />
+      <AppointmentList />
+    </main>
+  );
 };
 
 export default Home;
