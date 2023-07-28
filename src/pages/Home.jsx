840ffb3b697d77d40e76e5ad9d582d1460b03f33
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-07-28 15:37:36 +0300

    11.  Updated Home.jsx (appointments state)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import Doctors from "../components/Doctors";
 import AppointmentList from "../components/AppointmentList";
-import { doctorData } from "../helper/data";
+import { doctorData, appointmentData } from "../helper/data";
 
 const Home = () => {
   const [doctors, SetDoctors] = useState(doctorData);
+  const [appointments, setAppointments] = useState(appointmentData);
+  console.log("9", appointments);
 
   return (
     <main className="text-center mt-2 vh-100">
       <h1 className="display-5 text-success">MAK HOSPITAL</h1>
-      <Doctors doctors={doctors} />
-      <AppointmentList />
+      <Doctors
+        doctors={doctors}
+        appointments={appointments}
+        setAppointments={setAppointments}
+      />
+      <AppointmentList
+        appointments={appointments}
+        setAppointments={setAppointments}
+      />
     </main>
   );
 };
