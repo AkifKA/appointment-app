ea94ce4d27cefc017623c6ca0b78a7c9c75843bb
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-07-28 12:40:35 +0300

    7. updated Doctors.jsx

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,9 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useState } from "react";
+import AddModal from "./AddModal";
 
 const Doctors = ({ doctors }) => {
-  console.log(doctors);
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(true);
+  };
   return (
     <Container className="p-2">
       <h3 className="display-6 text-primary">Our Doctors</h3>
@@ -21,12 +27,14 @@ const Doctors = ({ doctors }) => {
               src={dr.img}
               alt="dr.name"
               className="img-thumbnail doctor-img"
+              onClick={handleClick}
             />
             <h5>{dr.name}</h5>
             <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
+      <AddModal show={show} handleClose={() => setShow(false)} />
     </Container>
   );
 };
